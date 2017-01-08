8b682133780acd88d816f99a76a578d8a09edb56
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2017-01-08 00:58:28 -0200

    Remove warnings in tests

--- Rakefile
+++ Rakefile
@@ -2,7 +2,7 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |t|
   t.libs = ['lib']
-  t.warning = true
+  t.warning = false
   t.verbose = false
   t.test_files = FileList['test/*_test.rb']
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,24 +1,3 @@
-require 'minitest/autorun'
+require 'minitest/reporters'
 
-# http://mislav.net/2011/06/ruby-verbose-mode/
-unless Kernel.respond_to? :silence_warnings
-  module Kernel
-    def silence_warnings
-      with_warnings(nil) { yield }
-    end
-
-    def with_warnings(flag)
-      old_verbose = $VERBOSE
-      $VERBOSE = flag
-
-      yield
-    ensure
-      $VERBOSE = old_verbose
-    end
-  end
-end
-
-# Supress warnings generate by Tenjin
-silence_warnings do
-  require_relative '../lib/gerablog'
-end
+require_relative '../lib/gerablog'
